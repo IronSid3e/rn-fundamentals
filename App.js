1abc1e21c92bdd495ed1f90c3040b2d97a36b921
IronSid3e <omertahirustun05@hotmail.com>
2026-01-16 19:54:05 +0300

    Study: Listing items with using FlatList

--- App.js
+++ App.js
@@ -1,23 +1,34 @@
-import react from "react";
-import { StyleSheet, Text, View, Button} from "react-native";
+import React from "react";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function App() {
+  const courses = [
+    { name: "Angular", id: 1 },
+    { name: "React JS", id: 2 },
+    { name: "C#", id: 3 },
+    { name: "C++", id: 4 },
+    { name: "Bootstrap", id: 5 },
+  ];
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Hello React Native </Text>
-      <Text>Java Script </Text>
-    </View>
+    <SafeAreaView>
+      <FlatList
+        data={courses}
+        //showsHorizontalScrollIndicator = {false} // Kaydırma çubuğunu görünmez yapar
+        keyExtractor={(item) => item.id} //itemlere id verir
+        renderItem={({ item }) => {
+          return <Text style={styles.content}>{item.name}</Text>; //arraydeki her itemi sırayla yazdırır
+        }}
+      ></FlatList>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: "#fff",
+  content: {
+    fontSize: 20,
+    backgroundColor: "yellow",
+    marginVertical: 5,
+    padding: 10,
   },
-  title: {
-    fontSize: 25,
-  }
 });
