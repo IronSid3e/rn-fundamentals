7eafdbda97fb03bcf958dd9b282d0a41a4c8d3a5
IronSid3e <omertahirustun05@hotmail.com>
2026-01-16 19:03:11 +0300

    Initial commit: Project setup

--- App.js
+++ App.js
@@ -1,20 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Enter text here" />
+        <Button title="Submit" />
+      </View>
+      <View>
+        <Text>Hello, World!</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 10,
+    padding: 8,
   },
 });
