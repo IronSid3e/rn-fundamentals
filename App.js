2b44dc2522e6a7e237d3e0a1b0e5426556750d9f
IronSid3e <omertahirustun05@hotmail.com>
2026-01-18 19:52:27 +0300

    useState: I did same example with useReducer  which I used useState before

--- App.js
+++ App.js
@@ -1,17 +1,39 @@
-import React, {useState} from "react";
+import React, { useReducer } from "react";
 import { StyleSheet, View, Text, Button } from "react-native";
 
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + action.payload };
+    case "decrement":
+      return { ...state, count: state.count - action.payload };
+    default:
+      return state;
+  }
+};
+//state == {count:0}
+//action == {type:'increment' || type:'decrement', payload}
+
 export default function App() {
-  const [counter, setCounter] = useState(0)
+  //const [counter, setCounter] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { count: 0 });
   return (
-    <View style = {styles.container}>
-      <Text style = {{padding: '25'}}>Sayı: {counter} </Text>
-      <Button title = "Arttır" onPress={()=> {
-        setCounter(counter + 1)
-      }}/>
-      <Button title = "Azalt" onPress={()=> {
-        setCounter(counter - 1)
-      }}/>
+    <View style={styles.container}>
+      <Text style={{ padding: 25 }}>Sayı: {state.count} </Text>
+      <Button
+        title="Arttır"
+        onPress={() => {
+          //setCounter(counter + 1)
+          dispatch({ type: "increment", payload: 1 });
+        }}
+      />
+      <Button
+        title="Azalt"
+        onPress={() => {
+          //setCounter(counter - 1);
+          dispatch({ type: "decrement", payload: 1 });
+        }}
+      />
     </View>
   );
 }
@@ -19,8 +41,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
     justifyContent: "center",
-    backgroundColor: '#5c5c5c',
+    backgroundColor: "#5c5c5c",
   },
 });
